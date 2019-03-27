1231c93e107943f3fa25d37080486106e9196e77
visualstudio <visualstudio@122-PC38>
2019-03-27 12:17:31 -0400

    call back

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp11
+namespace ConsoleApp1
 {
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
+            Console.WriteLine("downloading file");
+
+            Download();
+
+            Console.ReadLine();
+
+
+
         }
+
+
+
+        static void Download()
+
+        {
+
+            Network.Download((message) => Console.WriteLine("Download complete " + message));
+
+        }
+
     }
+
+
+
+    // Imaginary external network library
+
+
+
+    class Network
+
+    {
+
+        static public void Download(Action callback)
+
+        {
+
+            Task.Run(() => {
+
+                Thread.Sleep(3000);
+
+                callback("completed");
+
+            });
+
+        }
+
+    }
+
 }
